c509413e5fe6df20204e8f5337c73a096abcc3d3
Pilubou <labrujacosmica1970@gmail.com>
2024-12-05 15:17:09 -0300

    Update ListaCircular.c

--- ListaCircular.c
+++ ListaCircular.c
@@ -1,42 +1,54 @@
-typedef struct sNodo
+void crearLista(tLista * pl)
 {
-   void * dato;
-   unsigned tam;
-   struct sNodo * sig;
-
-}tNodo;
-
-typedef tLista * tNodo;
-
-void CrearLista(tLista * pl)
-{
-   *pl = NULL;
+    *pl = NULL;
 }
-int insertarEnOrden(tLista * pl, void * dato, unsigned tamDato, Cmp cmp)
-{
-   int rc;
-   tNodo * nue = (tNodo*) malloc(sizeof(tNodo));
-   void * datoN = malloc(tamDato);
-   if(!nue || !datoN)
-   {
-     free(nue);
-     free(datoN);
-     return SIN_MEM;
-   }
-   memcpy(datoN, dato, tamDato);
-   nue->dato = datoN;
-   nue->tam = tamDato;
-   if(!*pl) ///Caso de primer nodo a insertar
-   {
-     nue -> sig = nue;
-     *pl = nue;
-     return TODO_OK;
-   }
-   while(*pl && (rc = cmp (dato, (*pl)->dato)) < 0)
-   {
-      pl = &(*pl)->sig;
-   }
-   
- 
 
+int insertarEnOrden(tLista *pl, void * dato, unsigned tamDato, int (*cmp)(const void *, const void *))
+{
+    tNodo * nue = (tNodo *)malloc(sizeof(tNodo));
+    if(!nue || !(nue->dato = malloc(tamDato)))
+    {
+        free(nue);
+        return 0;
+    }
+    memcpy(nue->dato, dato, tamDato);
+    nue->tamDato = tamDato;
+    if(!*pl)
+    {
+        nue->sig = nue;
+        *pl = nue;
+        return 1;
+    }
+    tNodo *act = *pl;
+    int comp = cmp(act->dato,nue->dato);
+    if(comp > 0)
+    {
+        nue->sig = act;
+        act = act->sig;
+        while(act->sig != *pl)
+            act = act->sig;
+        act->sig = nue;
+        *pl = nue;
+        return 1;
+    }
+    tLista *lista = &(*pl)->sig; // Comenzar con el siguiente nodo
+    while (*lista != *pl && cmp((*lista)->dato, nue->dato) < 0)
+        lista = &(*lista)->sig;
+
+    nue->sig = *lista; // El nuevo nodo apunta al siguiente
+    *lista = nue;      // Enlazar el nodo actual al nuevo
+    return 1;
+}
+void Mostrar_Lista(tLista *pl, void (*mostrar)(const void *))
+{
+    if (!*pl) // Si la lista está vacía, no hay nada que mostrar
+        return;
+
+    tNodo *aux = *pl;
+    while(aux->sig != *pl)
+    {
+        mostrar(aux->dato);
+        aux = aux->sig;
+    }
+    mostrar(aux->dato);
 }
